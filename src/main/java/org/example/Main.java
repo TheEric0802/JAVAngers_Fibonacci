6b27aac14c49b310bfa4b53ecb71836ea5007eec
Eric Schmidt <eric0802@web.de>
2025-03-27 14:26:49 +0100

    add Exceptions for negative numbers and floatingpoint numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,33 @@ public class Main {
 
     public static int fibonacci(int n) {
 
-        if (n <= 2) {
-            return n-1;
+        if (n <= 1) {
+            if (n < 0) {
+                throw new IllegalArgumentException("n must be positive");
+            }
+            return n;
         }
 
         int num1 = 0;
         int num2 = 1;
         int result = 0;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             result = num1 + num2;
             num1 = num2;
             num2 = result;
         }
 
         return result;
+    }
+
+
+    public static int fibonacci(double n) {
+        if (n % 1 == 0) {
+            return fibonacci((int) n);
+        } else {
+            throw new IllegalArgumentException("n must be an integer");
+        }
     }
 
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,22 +8,42 @@ class MainTest {
 
     @Test
     void fibonacci_shouldReturn0_whenNIs1() {
-        assertEquals(0, Main.fibonacci(1));
+        assertEquals(0, Main.fibonacci(0));
     }
 
     @Test
     void fibonacci_shouldReturn1_whenNIs2() {
-        assertEquals(1, Main.fibonacci(2));
+        assertEquals(1, Main.fibonacci(1));
     }
 
     @Test
     void fibonacci_shouldReturn13_whenNIs8() {
-        assertEquals(13, Main.fibonacci(8));
+        assertEquals(13, Main.fibonacci(7));
     }
 
     @Test
     void fibonacci_shouldReturn21_whenNIs9() {
-        assertEquals(21, Main.fibonacci(9));
+        assertEquals(21, Main.fibonacci(8));
+    }
+
+    @Test
+    void fibonacci_shouldReturn21_whenNIs8_0() {
+        assertEquals(21, Main.fibonacci(8.0));
+    }
+
+    @Test
+    void fibonacci_shouldReturnError_whenNIsFloatingPointNumber() {
+        assertThrows(IllegalArgumentException.class, () -> Main.fibonacci(9.1));
+    }
+
+    @Test
+    void fibonacci_shouldReturnError_whenNIsNegative() {
+        assertThrows(IllegalArgumentException.class, () -> Main.fibonacci(-5));
+    }
+
+    @Test
+    void fibonacci_shouldReturnError_whenNIsNegativeFloatingPointNumber() {
+        assertThrows(IllegalArgumentException.class, () -> Main.fibonacci(-5.2));
     }
 
 }
